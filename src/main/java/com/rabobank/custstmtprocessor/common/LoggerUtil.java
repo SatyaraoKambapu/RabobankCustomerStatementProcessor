34dcdc2c320d02589ffb14b3f9a1f121c8038fac
satyarao kambapu <skambapu@altimetrik.com>
2018-07-15 10:00:37 +0530

    Used custom logging properties over than JVM logging properties

--- src/main/java/com/rabobank/custstmtprocessor/common/LoggerUtil.java
+++ src/main/java/com/rabobank/custstmtprocessor/common/LoggerUtil.java
@@ -1,10 +1,12 @@
 package com.rabobank.custstmtprocessor.common;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 /**
@@ -20,10 +22,8 @@ public class LoggerUtil {
 
 	private LoggerUtil() {
 		try {
-			/*
-			 * LogManager.getLogManager().readConfiguration( new
-			 * FileInputStream("mylogging.properties"));
-			 */
+			LogManager.getLogManager().readConfiguration(
+					new FileInputStream("./src/main/java/resources/rabobanklogging.properties"));
 			logger.setLevel(Level.FINE);
 			logger.addHandler(new ConsoleHandler());
 
