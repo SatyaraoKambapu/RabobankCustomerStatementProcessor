43cc44e67edb9329c06f3f389d7eb3e02da58768
satyarao kambapu <skambapu@altimetrik.com>
2018-07-15 07:42:03 +0530

    Added few more test cases for Reader classes.

---
+++ src/test/java/com/rabobank/custstmtprocessor/readers/test/XmlFileReaderTest.java
@@ -0,0 +1,42 @@
+package com.rabobank.custstmtprocessor.readers.test;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rabobank.custstmtprocessor.common.FileReader;
+import com.rabobank.custstmtprocessor.exception.BusinessOperationException;
+import com.rabobank.custstmtprocessor.readers.CsvFileReader;
+
+public class XmlFileReaderTest {
+	FileReader fileReader = null;
+
+	@Before
+	public void setUp() {
+		fileReader = new CsvFileReader();
+	}
+
+	@After
+	public void cleanUp() {
+
+	}
+
+	@Test
+	public void testProcessXmlInputFile() throws BusinessOperationException {
+		String csvpath = ".\\src\\test\\java\\com\\rabobank\\custstmtprocessor\\resources\\test\\records.xml";
+		File inputF = new File(csvpath);
+		Assert.assertEquals(10, fileReader.processInputFile(inputF).size());
+	}
+
+	@Test(expected = BusinessOperationException.class)
+	public void testProcessNonXmlInputFile() throws BusinessOperationException {
+		String csvpath = ".\\src\\test\\java\\com\\rabobank\\custstmtprocessor\\resources\\test\\records.csv";
+		File inputF = new File(csvpath);
+		fileReader.processInputFile(inputF);
+	}
+
+}
